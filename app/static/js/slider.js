ecffcff21f9f24dea0743c2672703245d04e16ed
wenbinzhou <330759294@qq.com>
2015-01-19 20:34:01 +0800

    图片滚动效果更改

--- app/static/js/slider.js
+++ app/static/js/slider.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    items:1
-    })
+window.mySwipe = new Swipe(document.getElementById('slider'), {
+  startSlide: 1,
+  speed: 400,
+  auto: 0,
+  continuous: true,
+  disableScroll: false,
+  stopPropagation: false,
+  callback: function(index, elem) {
+    $('.swipe-dot span').eq(index).addClass('active').siblings().removeClass('active');
+  },
+  transitionEnd: function(index, elem) {}
 });
