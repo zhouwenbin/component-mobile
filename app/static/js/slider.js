90feee609f1cf863efcd9fad94904468632815c6
wenbinzhou <330759294@qq.com>
2015-01-22 12:12:31 +0800

    添加首页

--- app/static/js/slider.js
+++ app/static/js/slider.js
@@ -1,5 +1,14 @@
+$(function(){
+  var length=$(".swipe-wrap>div").length;
+  var swipeDot='';
+  for(var i=0;i<length;i++){
+    swipeDot+='<span></span>'
+  }
+  $('.swipe-dot').append(swipeDot);
+  $('.swipe-dot span').eq(0).addClass('active');
+})
 window.mySwipe = new Swipe(document.getElementById('slider'), {
-  startSlide: 1,
+  startSlide: 0,
   speed: 400,
   auto: 0,
   continuous: true,
