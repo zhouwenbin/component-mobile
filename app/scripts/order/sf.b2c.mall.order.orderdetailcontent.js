a2cf03a3838ced599f0bd109ec821e3297064737
jnotnull <252210289@qq.com>
2015-01-27 18:09:00 +0800

    [dev]订单详情
    订单详情

--- app/scripts/order/sf.b2c.mall.order.orderdetailcontent.js
+++ app/scripts/order/sf.b2c.mall.order.orderdetailcontent.js
@@ -4,6 +4,7 @@ define('sf.b2c.mall.order.orderdetailcontent', [
     'can',
     'zepto',
     'sf.b2c.mall.api.order.getOrder',
+    'sf.b2c.mall.api.order.cancelOrder',
     'sf.b2c.mall.api.user.updateReceiverInfo',
     'sf.b2c.mall.api.user.getIDCardUrlList',
     'sf.b2c.mall.api.sc.getUserRoutes',
@@ -18,7 +19,7 @@ define('sf.b2c.mall.order.orderdetailcontent', [
     'sf.b2c.mall.widget.message',
     'moment'
   ],
-  function(can, $, SFGetOrder, SFUpdateReceiverInfo, SFGetIDCardUrlList, SFGetUserRoutes, SFGetRecvInfo, SFConfirmReceive, loading, FrameworkComm, SFConfig, Utils, helpers, SFOrderFn, SFMessage, moment) {
+  function(can, $, SFGetOrder, SFCancelOrder, SFUpdateReceiverInfo, SFGetIDCardUrlList, SFGetUserRoutes, SFGetRecvInfo, SFConfirmReceive, loading, FrameworkComm, SFConfig, Utils, helpers, SFOrderFn, SFMessage, moment) {
 
     return can.Control.extend({
 
@@ -75,71 +76,16 @@ define('sf.b2c.mall.order.orderdetailcontent', [
             that.options.orderId = data.orderId;
             that.options.recId = data.orderItem.rcvrId;
 
-            //data.orderItem.orderStatus = "SHIPPED";
+            //data.orderItem.orderStatus = "SUBMITED";
             //data.orderItem.rcvrState = 0
             that.options.status = that.statsMap[data.orderItem.orderStatus];
-            that.options.nextStep = that.optionHTML[that.nextStepMap[data.orderItem.orderStatus]];
-            that.options.currentStepTips = that.currentStepTipsMap[data.orderItem.orderStatus];
+            that.options.operationHTML = that.optionHTML[that.nextStepMap[data.orderItem.orderStatus]];
 
-            that.options.showStep = true;
-            if (data.orderItem.orderStatus == 'AUTO_CANCEL' || data.orderItem.orderStatus == 'USER_CANCEL' || data.orderItem.orderStatus == 'OPERATION_CANCEL') {
-              that.options.showStep = false;
-            }
-
-            that.options.user = new can.Map();
-            that.options.IDCard = {};
-            that.options.IDCard.needUpload = (data.orderItem.rcvrState == 0 || data.orderItem.rcvrState == 1 || data.orderItem.rcvrState == 3);
-            that.options.IDCard.state = data.orderItem.rcvrState;
-            if (that.options.IDCard.needUpload) {
-              $('#uploadidcard').show();
-              //读取身份证的状态
-              that.options.currentStepTips = that.cardTipsMap[idcard.status] || '';
-              that.options.currentStatus = that.cardStatusMap[idcard.status] || '';
+            if (data.orderItem.rcvrState == 0 || data.orderItem.rcvrState == 1 || data.orderItem.rcvrState == 3) {
+              that.options.uploadIDCardTips = '该笔订单需要上传收货人身份信息，请<a href="">联系客服</a>';
             }
 
             that.options.traceList = data.orderActionTraceItemList;
-
-            var map = {
-              'SUBMITED': function(trace) {
-                that.options.submitedTime = trace.gmtHappened;
-                that.options.submitedActive = "active";
-              },
-
-              'AUDITING': function(trace) {
-                that.options.auditingTime = trace.gmtHappened;
-                that.options.auditingActive = "active";
-              },
-              'BUYING': function(trace) {},
-              'WAIT_SHIPPING': function(trace) {},
-
-              'SHIPPING': function(trace) {
-                that.options.wait_shippingTime = trace.gmtHappened;
-                that.options.wait_shippingActive = "active";
-              },
-
-              'SHIPPED': function(trace) {
-                that.options.shippingTime = trace.gmtHappened;
-                that.options.shippingActive = "active";
-              },
-
-              'COMPLETED': function(trace) {
-                that.options.completedTime = trace.gmtHappened;
-                that.options.completedActive = "active";
-              },
-
-              'AUTO_COMPLETED': function(trace) {
-                that.options.completedTime = trace.gmtHappened;
-                that.options.completedActive = "active";
-              }
-            }
-            _.each(that.options.traceList, function(trace) {
-              trace.operator = that.operatorMap[trace.operator] || '系统';
-              trace.description = that.statusDescription[trace.status];
-
-              if (typeof map[trace.status] != 'undefined') {
-                map[trace.status].call(that, trace);
-              }
-            })
 
             //加入订单状态
             _.each(that.options.traceList, function(trace) {
@@ -175,7 +121,7 @@ define('sf.b2c.mall.order.orderdetailcontent', [
             _.each(that.options.productList, function(item) {
               item.totalPrice = item.price * item.quantity;
               if (typeof that.options.productList[0].spec !== "undefined") {
-                item.spec = that.options.productList[0].spec.split(',').join("&nbsp;/&nbsp;");
+                item.spec = that.options.productList[0].spec.split(',').join("</li><li>");
               }
               if (item.imageUrl == "" || item.imageUrl == null) {
                 item.imageUrl = "http://www.sfht.com/img/no.png";
@@ -185,7 +131,6 @@ define('sf.b2c.mall.order.orderdetailcontent', [
             });
 
             that.options.allTotalPrice = that.options.productList[0].totalPrice;
-            //that.options.allTotalPrice = that.options.productList[0].totalPrice;
 
             var cancelArr = new Array();
             cancelArr.push('AUTO_CANCEL');
@@ -207,33 +152,26 @@ define('sf.b2c.mall.order.orderdetailcontent', [
             that.options.showTax = that.options.productList[0].bonded;
             that.options.shouldPayPrice = that.options.allTotalPrice;
 
+            if (data.orderItem.orderStatus == 'SUBMITED'){
+              that.options.showPayButton = true;
+            }
+
             var html = can.view('templates/order/sf.b2c.mall.order.orderdetail.mustache', that.options);
             that.element.html(html);
 
-            if (that.options.IDCard.needUpload) {
-              that.component.loading = new loading('.sf-b2c-mall-loading');
-              that.setPhotoP();
-              that.setPhotoN();
+            $('#gotopay').tap(function(){
+              that.payclick($(this));
+            })
 
-              if (null != idcard.credtImgUrl1 && "" != idcard.credtImgUrl1) {
-                // $('#leftTip').empty();
-                that.options.user.attr('credtImgUrl1', idcard.credtImgUrl1);
-                $('#file-submit-input-photo-p img').attr('src', that.getUserPhotoUrl({
-                  n: idcard.credtImgUrl1
-                }))
-              }
+            $('#cancelorder').tap(function(){debugger;
+              that.cancelOrderClick($(this));
+            })
 
-              if (null != idcard.credtImgUrl2 && "" != idcard.credtImgUrl2) {
-                // $('#rightTip').empty();
-                that.options.user.attr('credtImgUrl2', idcard.credtImgUrl2);
-                $('#file-submit-input-photo-n img').attr('src', that.getUserPhotoUrl({
-                  n: idcard.credtImgUrl2
-                }))
-              }
-            }
+            $('.loadingDIV').hide();
           })
           .fail(function(error) {
             console.error(error);
+            $('.loadingDIV').hide();
           })
       },
 
@@ -247,128 +185,6 @@ define('sf.b2c.mall.order.orderdetailcontent', [
         0: "<span class='label label-error'>尊敬的客户，该笔订单清关时需要上传收货人的身份证照片，为了您更快的收到商品，请尽快上传收货人的身份证照片。</span>",
         1: "<span class='label label-error'>尊敬的客户，您上传的身份证照片正在审核中，请耐心等待。</span>",
         3: "<span class='label label-error'>尊敬的客户，您上传的身份证照片审核不通过，请重新上传！为了您更快的收到商品，请尽快上传正确的身份证照片。</span>"
-      },
-
-      getUserPhotoUrl: function(param) {
-        var data = Utils.sign({
-          level: 'USERLOGIN',
-          data: {
-            n: param.n
-          }
-        })
-        return SFConfig.setting.api.fileurl + '?' + $.param(data);
-      },
-
-      setError: function(errorText) {
-        //this.options.error.attr('errorText', errorText);
-        this.element.find('.error-tips').text(errorText);
-      },
-
-      setPhotoP: function() {
-
-        this.component.uploaderPhotoP = new FileUploader();
-
-        var that = this;
-        var callback = {
-          onUploadSuccess: function(obj, data) {
-
-            that.component.loading.hide();
-            $('.error-tips').empty();
-            $('#leftTip').empty();
-
-            var img = data.content[0][that.cardPUpname];
-            that.options.user.attr('credtImgUrl1', img);
-
-            $('#file-submit-input-photo-p img').attr('src', that.getUserPhotoUrl({
-              n: img
-            }))
-
-            that.component.uploaderPhotoP.reset();
-          },
-
-          onUploadError: function(data) {
-            that.component.loading.hide();
-            if (data.status === 413) {
-              that.setError.call(that, that.defaults.alert);
-            }
-
-            that.component.uploaderPhotoP.reset();
-          },
-
-          onUploadBeforeSend: function(obj, data) {
-            var filename = obj.file.name;
-            that.cardPUpname = 'ID_CARD_1' + filename.substring(filename.lastIndexOf('.'), filename.length);
-            that.component.uploaderPhotoP.setFileVal(that.cardPUpname);
-            that.component.loading.show();
-          },
-
-          onError: function(errorCode) {
-            that.component.uploaderPhotoP.reset();
-            var map = {
-              'Q_TYPE_DENIED': '电子照上传失败，选取的文件类型不支持',
-              'F_EXCEED_SIZE': '电子照上传失败，大小请控制在5M以内'
-            }
-
-            var errorText = map[errorCode] || that.defaults.alert;
-            that.setError.call(that, errorText);
-          }
-        };
-
-        this.component.uploaderPhotoP.config({
-          pick: '#file-submit-input-photo-p'
-        }, callback);
-      },
-
-      setPhotoN: function() {
-
-        this.component.uploaderPhotoN = new FileUploader();
-
-        var that = this;
-        var callback = {
-          onUploadSuccess: function(obj, data) {
-            that.component.loading.hide();
-            $('.error-tips').empty();
-            $('#rightTip').empty();
-            var img = data.content[0][that.cardPUpname];
-            that.options.user.attr('credtImgUrl2', img);
-            $('#file-submit-input-photo-n img').attr('src', that.getUserPhotoUrl({
-              n: img
-            }))
-
-            that.component.uploaderPhotoN.reset();
-          },
-
-          onUploadError: function(data) {
-            that.component.loading.hide();
-            if (data.status === 413) {
-              that.setError.call(that, that.defaults.alert);
-            }
-
-            that.component.uploaderPhotoN.reset();
-          },
-
-          onUploadBeforeSend: function(obj, data) {
-            var filename = obj.file.name;
-            that.cardPUpname = 'ID_CARD_2' + filename.substring(filename.lastIndexOf('.'), filename.length);
-            that.component.uploaderPhotoN.setFileVal(that.cardPUpname);
-            that.component.loading.show();
-          },
-
-          onError: function(errorCode) {
-            that.component.uploaderPhotoP.reset();
-            var map = {
-              'Q_TYPE_DENIED': '电子照上传失败，选取的文件类型不支持',
-              'F_EXCEED_SIZE': '电子照上传失败，大小请控制在5M以内'
-            }
-
-            var errorText = map[errorCode] || that.defaults.alert;
-            that.setError.call(that, errorText);
-          }
-        };
-
-        this.component.uploaderPhotoN.config({
-          pick: '#file-submit-input-photo-n'
-        }, callback);
       },
 
       statusDescription: {
@@ -404,7 +220,7 @@ define('sf.b2c.mall.order.orderdetailcontent', [
       },
 
       optionHTML: {
-        "NEEDPAY": '<a href="#" class="btn btn-send" id="pay">立即支付</a>',
+        "CANCELORDER": '<a href="javascript:void(0)" id="cancelorder">取消订单</a>',
         "RECEIVED": '<a href="#" class="btn btn-send received">确认签收</a>'
       },
 
@@ -433,7 +249,7 @@ define('sf.b2c.mall.order.orderdetailcontent', [
       },
 
       nextStepMap: {
-        'SUBMITED': 'NEEDPAY',
+        'SUBMITED': 'CANCELORDER',
         'SHIPPED': 'RECEIVED'
       },
 
@@ -456,50 +272,40 @@ define('sf.b2c.mall.order.orderdetailcontent', [
         'AUTO_COMPLETED': '尊敬的用户，您的订单已经签收超过7天，已自动完成。期待您再次使用顺丰海淘'
       },
 
-      "#orderdetail-view click": function(element, event) {
-        $(".orderdetail-upload").show();
-        $(".mask2").show();
+      cancelOrderClick: function(element, event) {
+        var that = this;
+
+        var message = new SFMessage(null, {
+          'tip': '确认要取消该订单？',
+          'type': 'confirm',
+          'okFunction': _.bind(that.cancelOrder, that, element)
+        });
+
         return false;
       },
 
-      "#orderdetail-save click": function(element, event) {
-
+      cancelOrder: function(element) {
         var that = this;
-        var user = this.options.user.attr();
+        var orderid = element.parent('div#operationarea').eq(0).attr('data-orderid');
+        var cancelOrder = new SFCancelOrder({
+          "orderId": orderid
+        });
 
-        if (typeof user.credtImgUrl1 == 'undefined' || user.credtImgUrl1 == null) {
-          alert("请上传身份证正面！");
-          return false;
-        }
-        if (typeof user.credtImgUrl2 == 'undefined' || user.credtImgUrl2 == null) {
-          alert("请上传身份证反面！");
-          return false;
-        }
-
-        var person = {};
-        person.credtImgUrl1 = user.credtImgUrl1;
-        person.credtImgUrl2 = user.credtImgUrl2;
-        person.recId = this.options.recId;
-
-        var updateReceiverInfo = new SFUpdateReceiverInfo(person);
-        updateReceiverInfo
+        cancelOrder
           .sendRequest()
           .done(function(data) {
-            alert("保存成功");
+            new SFMessage(null, {
+              'tip': '订单取消成功！',
+              'type': 'success'
+            });
             that.render();
           })
           .fail(function(error) {
-            console.error(error)
-          });
-
-        return false;
-      },
-
-      "#closeDialog click": function(element, event) {
-
-        $(element).parents(".register").hide(300);
-        $(".mask").hide();
-        return false;
+            new SFMessage(null, {
+              'tip': '订单取消失败！',
+              'type': 'error'
+            });
+          })
       },
 
       '.received click': function(element, event) {
@@ -541,7 +347,7 @@ define('sf.b2c.mall.order.orderdetailcontent', [
           })
       },
 
-      '#pay click': function(element, event) {
+      payclick: function(element, event) {
         event && event.preventDefault();
         var that = this;
         var callback = {
