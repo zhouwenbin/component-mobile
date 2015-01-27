263b34feddaa38dffed95b48b4ecb644ee88855d
TimGuan <gkqwt1990@gmail.com>
2015-01-27 10:44:56 +0800

    [FIX]修改首页js

--- app/scripts/page/sf.b2c.mall.page.main.js
+++ app/scripts/page/sf.b2c.mall.page.main.js
@@ -24,6 +24,21 @@ define(
         //   onLogin: _.bind(this.onLogin, this)
         // });
         // new Footer('.sf-b2c-mall-footer');
+        //滚动效果
+        new Swipe($('#slider')[0], {
+          startSlide: 0,
+          speed: 400,
+          auto: 0,
+          continuous: true,
+          disableScroll: false,
+          stopPropagation: false,
+          callback: function(index, elem) {
+            $('.swipe-dot span').eq(index).addClass('active').siblings().removeClass('active');
+          },
+          transitionEnd: function(index, elem) {}
+        });
+        //第一个选中
+        $('.swipe-dot span').eq(0).addClass('active');
         }
     });
     new home();
