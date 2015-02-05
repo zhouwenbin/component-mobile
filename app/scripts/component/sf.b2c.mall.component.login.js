c778354231512432f33fd4c9529d8fcce97dea7f
aicekiller <1017634520@qq.com>
2015-02-05 10:55:58 +0800

    登陆冗余代码删除

--- app/scripts/component/sf.b2c.mall.component.login.js
+++ app/scripts/component/sf.b2c.mall.component.login.js
@@ -17,8 +17,6 @@ define('sf.b2c.mall.component.login', [
     var DEFAULT_CAPTCHA_ID = 'haitaob2c';
     var DEFAULT_CAPTCHA_HASH = '5f602a27181573d36e6c9d773ce70977';
 
-    var COUNT = 0;
-
     var ERROR_NO_INPUT_USERNAME = '请输入顺丰海淘帐号';
     var ERROR_INPUT_USERNAME = '账户有误，请重新输入';
     var ERROR_NO_INPUT_PWD = '请输入密码';
@@ -57,8 +55,8 @@ define('sf.b2c.mall.component.login', [
           verifiedCodeUrl: null,
           autologin: false,
           sessionId: null,
-          platform: params.platform || (SFFn.isMobile.any() ? 'mobile' : null),
-          isPlaceholderSupport: this.isPlaceholderSupport()
+          platform: params.platform || (SFFn.isMobile.any() ? 'mobile' : null)
+          //isPlaceholderSupport: this.isPlaceholderSupport()
         })
 
         this.render(this.data);
@@ -69,17 +67,17 @@ define('sf.b2c.mall.component.login', [
           that.loginButtonClick();
         })
 
+        // 去注册
         $("#toRegist").tap(function() {
-
           var params = can.deparam(window.location.search.substr(1));
           var gotoUrl = params.from;
           window.location.href = 'http://m.sfht.com/register.html?from=' + escape(gotoUrl);
         })
       },
 
-      isPlaceholderSupport: function() {
-        return 'placeholder' in document.createElement('input');
-      },
+      // isPlaceholderSupport: function() {
+      //   return 'placeholder' in document.createElement('input');
+      // },
 
       /**
        * @description 渲染页面
@@ -88,12 +86,6 @@ define('sf.b2c.mall.component.login', [
       render: function(data) {
         var html = can.view('templates/component/sf.b2c.mall.component.login.mustache', data, this.helpers);
         this.element.append(html);
-
-        if (COUNT >= 3) {
-          this.data.attr('isNeedVerifiedCode', true);
-        }
-
-        this.funPlaceholder(document.getElementById('user-name'));
       },
       /**
        * @description 验证码更换
@@ -118,7 +110,7 @@ define('sf.b2c.mall.component.login', [
        */
       '#verified-code-btn click': function(element, event) {
         event && event.preventDefault();
-        this.getVerifiedCode()
+        this.getVerifiedCode();
       },
       /**
        * @description 用户名验证
@@ -128,14 +120,14 @@ define('sf.b2c.mall.component.login', [
       checkUserName: function(username) {
         var username = $.trim(username);
         var isTelNum = /^1\d{10}$/.test(username);
-        var isEmail = /^([a-zA-Z0-9-_]*[-_\.]?[a-zA-Z0-9]+)*@([a-zA-Z0-9]*[-_]?[a-zA-Z0-9]+)+[\.][a-zA-Z]{2,3}([\.][a-zA-Z]{2})?$/.test(username);
+        //var isEmail = /^([a-zA-Z0-9-_]*[-_\.]?[a-zA-Z0-9]+)*@([a-zA-Z0-9]*[-_]?[a-zA-Z0-9]+)+[\.][a-zA-Z]{2,3}([\.][a-zA-Z]{2})?$/.test(username);
         if (!username) {
           this.element.find('#username-error-tips').text(ERROR_NO_INPUT_USERNAME).show();
           return false;
-        } else if (username.length > 30) {
+        } else if (username.length > 11) {
           this.element.find('#username-error-tips').text(ERROR_INPUT_USERNAME).show();
           return false;
-        } else if (!isTelNum && !isEmail) {
+        } else if (!isTelNum) {
           this.element.find('#username-error-tips').text(ERROR_INPUT_USERNAME).show();
           return false;
         } else {
@@ -191,7 +183,7 @@ define('sf.b2c.mall.component.login', [
         });
         this.component.needVfCode.sendRequest()
           .done(function(data) {
-            if (data.value) {
+            if (data.value == true) {
               that.data.attr('isNeedVerifiedCode', true);
             } else {
               that.data.attr('isNeedVerifiedCode', false);
@@ -202,57 +194,31 @@ define('sf.b2c.mall.component.login', [
           })
       },
       /**
-       * @description 修复ie7,8,9placeholder bug
-       * @param  {String}
-       * @return {String}
-       */
-      funPlaceholder: function(element) {
-        var placeholder = '';
-        if (element && !("placeholder" in document.createElement("input")) && (placeholder = element.getAttribute("placeholder"))) {
-          element.onfocus = function() {
-            if (this.value === placeholder) {
-              this.value = "";
-            }
-          };
-
-          element.onblur = function() {
-            if (this.value === "") {
-              this.value = placeholder;
-            }
-          };
-
-          if (element.value === "") {
-            element.value = placeholder;
-          }
-
-        }
-      },
-      /**
        * @description 通过正则表达式检查账号类型
        * @param  {String} account 账号
        * @return {String} 返回MAIL或者MOBILE
        */
-      checkTypeOfAccount: function(account) {
+      // checkTypeOfAccount: function(account) {
 
-        account = $.trim(account);
+      //   var account = $.trim(account);
 
-        // 检查账号的类型返回MOBILE或者MAIL
-        var isTelNum = /^1\d{10}$/.test(account);
-        var isEmail = /^([a-zA-Z0-9-_]*[-_\.]?[a-zA-Z0-9]+)*@([a-zA-Z0-9]*[-_]?[a-zA-Z0-9]+)+[\.][a-zA-Z]{2,3}([\.][a-zA-Z]{2})?$/.test(account);
-        if (isTelNum) {
-          return 'MOBILE';
-        }
-        if (isEmail) {
-          return 'MAIL';
-        }
-      },
+      //   // 检查账号的类型返回MOBILE或者MAIL
+      //   var isTelNum = /^1\d{10}$/.test(account);
+      //   //var isEmail = /^([a-zA-Z0-9-_]*[-_\.]?[a-zA-Z0-9]+)*@([a-zA-Z0-9]*[-_]?[a-zA-Z0-9]+)+[\.][a-zA-Z]{2,3}([\.][a-zA-Z]{2})?$/.test(account);
+      //   if (isTelNum) {
+      //     return 'MOBILE';
+      //   }
+      //   if (isEmail) {
+      //     return 'MAIL';
+      //   }
+      // },
 
       /**
        * @description 获得焦点之后对账号输入内容做检查
        * @param  {dom} element jquery dom对象
        * @param  {event} event event对象
        */
-      '.input-username keyup': function(element, event) {
+      '#user-name keyup': function(element, event) {
         event && event.preventDefault();
         this.element.find('#username-error-tips').hide();
       },
@@ -262,17 +228,16 @@ define('sf.b2c.mall.component.login', [
        * @param  {dom} element jquery dom对象
        * @param  {event} event event对象
        */
-      '.input-password keyup': function(element, event) {
+      '#user-pwd keyup': function(element, event) {
         event && event.preventDefault();
         this.element.find('#pwd-error-tips').hide();
-        $(element).siblings('label').hide();
       },
       /**
        * @description 失去焦点之后对账号输入内容做检查
        * @param  {dom} element jquery dom对象
        * @param  {event} event event对象
        */
-      '.input-username blur': function(element, event) {
+      '#user-name blur': function(element, event) {
         event && event.preventDefault();
 
         var username = this.data.attr('username');
@@ -286,14 +251,9 @@ define('sf.b2c.mall.component.login', [
        * @param  {dom} element jquery dom对象
        * @param  {event} event event对象
        */
-      '.input-password blur': function(element, event) {
+      '#user-pwd blur': function(element, event) {
         event && event.preventDefault();
-        var password = $(element).val();
-        if (password) {
-          $(element).siblings('label').hide();
-        } else {
-          $(element).siblings('label').show();
-        }
+        var password = this.data.attr('password');
         this.checkPwd.call(this, password);
 
       },
@@ -306,7 +266,7 @@ define('sf.b2c.mall.component.login', [
         $('#pwd-error-tips').hide();
       },
 
-      'input focus': function(element, event) {
+      '#verified-code focus': function(element, event) {
         $('#code-error-tips').hide();
       },
 
@@ -320,7 +280,7 @@ define('sf.b2c.mall.component.login', [
           .done(function(data) {
             if (data.userId) {
               that.data.attr('autologin')
-              store.set('type', that.checkTypeOfAccount(that.data.attr('username')));
+              store.set('type', 'MOBILE');
               store.set('nickname', that.data.attr('username'));
 
               // deparam过程 -- 从url中获取需要请求的sku参数
@@ -383,7 +343,7 @@ define('sf.b2c.mall.component.login', [
 
             this.component.login.setData({
               accountId: $.trim(this.data.attr('username')),
-              type: this.checkTypeOfAccount(this.data.attr('username')),
+              type: 'MOBILE',
               password: md5(this.data.attr('password') + SFConfig.setting.md5_key),
               vfCode: vfCode
             });
@@ -394,7 +354,7 @@ define('sf.b2c.mall.component.login', [
           if (this.checkUserName.call(this, username) && this.checkPwd.call(this, password)) {
             this.component.login.setData({
               accountId: $.trim(this.data.attr('username')),
-              type: this.checkTypeOfAccount(this.data.attr('username')),
+              type: 'MOBILE',
               password: md5(this.data.attr('password') + SFConfig.setting.md5_key)
             });
             that.sendRequest();
