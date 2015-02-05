e0952a395c47e6b957469ad1dac503e4f193f843
jnotnull <252210289@qq.com>
2015-02-05 17:36:00 +0800

    修改打包

--- app/scripts/component/sf.b2c.mall.component.login.js
+++ app/scripts/component/sf.b2c.mall.component.login.js
@@ -67,9 +67,9 @@ define('sf.b2c.mall.component.login', [
           that.loginButtonClick($(this));
         })
 
-        // $('.weixinlogin').tap(function() {
-        //   that.weixinLoginAuth();
-        // })
+        $('.weixinlogin').tap(function() {
+          that.weixinLoginAuth();
+        })
 
         $('#verified-code-btn').tap(function() {
           that.getVerifiedCode();
@@ -86,32 +86,32 @@ define('sf.b2c.mall.component.login', [
        * [weixinLogin 微信登陆]
        * @return {[type]} [description]
        */
-      // weixinLoginAuth: function() {
-      //   var reqLoginAuth = new SFReqLoginAuth({
-      //     "partnerId": "wechat_mp",
-      //     "redirectUrl": "http://www.sfht.com/weixincenter.html?from=m.sfht.com/weixincenter.html"
-      //   });
+      weixinLoginAuth: function() {
+        var reqLoginAuth = new SFReqLoginAuth({
+          "partnerId": "wechat_mp",
+          "redirectUrl": "http://www.sfht.com/weixincenter.html?from=m.sfht.com/weixincenter.html"
+        });
 
-      //   alert("begin6");
-      //   // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5fc1c50423d1d764&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
-      //   window.open("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5fc1c50423d1d764&response_type=code&scope=snsapi_userinfo&redirect_uri=http%3A%2F%2Fwww.sfht.com&state=STATE#wechat_redirect");
+        alert("begin7");
+        // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5fc1c50423d1d764&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+        window.open("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5fc1c50423d1d764&response_type=code&scope=snsapi_userinfo&redirect_uri=http%3A%2F%2Fwww.sfht.com&state=STATE#wechat_svm");
 
-      //   // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5fc1c50423d1d764&redirect_uri=http%3A%2F%2Fwww.sfht.com%2Findex.html%3Ffrom%3Dhttp%3A%2F%2Fm.sfht.com%2Fweixincenter.html%7Corder.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+        // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5fc1c50423d1d764&redirect_uri=http%3A%2F%2Fwww.sfht.com%2Findex.html%3Ffrom%3Dhttp%3A%2F%2Fm.sfht.com%2Fweixincenter.html%7Corder.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
 
-      //   reqLoginAuth
-      //     .sendRequest()
-      //     .done(function(data) {
+        reqLoginAuth
+          .sendRequest()
+          .done(function(data) {
 
-      //       store.set('weixinto', 'order.html');
-      //       alert("open loginauth:" + data.loginAuthLink);
-      //       // window.location.href = "" + data.loginAuthLink + "";
-      //       // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5fc1c50423d1d764&redirect_uri=http%3A%2F%2Fwww.sfht.com%2Findex.html%3Ffrom%3Dhttp%3A%2F%2Fm.sfht.com%2Fweixincenter.html%7Corder.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
-      //       return false;
-      //     })
-      //     .fail(function(error) {
-      //       console.error(error);
-      //     })
-      // },
+            store.set('weixinto', 'order.html');
+            alert("open loginauth:" + data.loginAuthLink);
+            // window.location.href = "" + data.loginAuthLink + "";
+            // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5fc1c50423d1d764&redirect_uri=http%3A%2F%2Fwww.sfht.com%2Findex.html%3Ffrom%3Dhttp%3A%2F%2Fm.sfht.com%2Fweixincenter.html%7Corder.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+            return false;
+          })
+          .fail(function(error) {
+            console.error(error);
+          })
+      },
 
       /**
        * @description 渲染页面
