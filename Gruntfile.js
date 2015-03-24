aaf550dc4ab44af00cec977439424bbaeafe505c
zhang.ke <zhang.ke@sf-express.com>
2015-03-24 15:35:38 +0800

    打包文件修改

--- Gruntfile.js
+++ Gruntfile.js
@@ -698,11 +698,13 @@ module.exports = function (grunt) {
           out: './<%= config.tmp %>/concat/scripts/sf.b2c.mall.h5.page.paysuccess.js',
           mainConfigFile: "./<%= config.app %>/scripts/sf.b2c.mall.require.config.js",
           paths: {
-            'sf.b2c.mall.business.config': 'scripts/config/sf.b2c.mall.business.<%= config.target %>.config'
+            'sf.b2c.mall.business.config': 'scripts/config/sf.b2c.mall.business.<%= config.target %>.config',
+            'moment': '../bower_components/momentjs/min/moment.min'
           },
           include: [
             "sf.b2c.mall.business.config",
-            'sf.b2c.mall.page.paysuccess'
+            'sf.b2c.mall.page.paysuccess',
+            'sf.helpers'
           ],
           insertRequire: ['sf.b2c.mall.page.paysuccess']
         }
