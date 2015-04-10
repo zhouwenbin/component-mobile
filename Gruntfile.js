e9d8b738e03ef4463544b302b6693a72eb95580d
zhang.ke <zhang.ke@sf-express.com>
2015-04-10 10:28:50 +0800

    [人品PK]领券页

--- Gruntfile.js
+++ Gruntfile.js
@@ -861,8 +861,25 @@ module.exports = function (grunt) {
           ],
           insertRequire: ['sf.b2c.mall.page.luckymoneyaccept']
         }
-      }
+      },
 
+      searchwarrior: {
+        options: {
+          preserveLicenseComments: false,
+          baseUrl: './app/',
+          out: './<%= config.tmp %>/concat/scripts/sf.b2c.mall.h5.page.searchwarrior.js',
+          mainConfigFile: "./<%= config.app %>/scripts/sf.b2c.mall.require.config.js",
+          paths: {
+            'moment': '../bower_components/momentjs/min/moment.min',
+            'sf.b2c.mall.business.config': 'scripts/config/sf.b2c.mall.business.<%= config.target %>.config'
+          },
+          include: [
+            "sf.b2c.mall.business.config",
+            'sf.b2c.mall.page.searchwarrior'
+          ],
+          insertRequire: ['sf.b2c.mall.page.searchwarrior']
+        }
+      }
     }
   });
 
