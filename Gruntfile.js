6557151790b88d476b114af2313e7549393d1e7e
aicekiller <1017634520@qq.com>
2015-04-13 20:09:54 +0800

    打包文件更新

--- Gruntfile.js
+++ Gruntfile.js
@@ -861,8 +861,43 @@ module.exports = function (grunt) {
           ],
           insertRequire: ['sf.b2c.mall.page.luckymoneyaccept']
         }
-      }
+      },
 
+      setpassword: {
+        options: {
+          preserveLicenseComments: false,
+          baseUrl: './app/',
+          out: './<%= config.tmp %>/concat/scripts/sf.b2c.mall.h5.page.setpassword.js',
+          mainConfigFile: "./<%= config.app %>/scripts/sf.b2c.mall.require.config.js",
+          paths: {
+            'moment': '../bower_components/momentjs/min/moment.min',
+            'sf.b2c.mall.business.config': 'scripts/config/sf.b2c.mall.business.<%= config.target %>.config'
+          },
+          include: [
+            "sf.b2c.mall.business.config",
+            'sf.b2c.mall.page.setpassword'
+          ],
+          insertRequire: ['sf.b2c.mall.page.setpassword']
+        }
+      },
+
+      bindaccount: {
+        options: {
+          preserveLicenseComments: false,
+          baseUrl: './app/',
+          out: './<%= config.tmp %>/concat/scripts/sf.b2c.mall.h5.page.bindaccount.js',
+          mainConfigFile: "./<%= config.app %>/scripts/sf.b2c.mall.require.config.js",
+          paths: {
+            'moment': '../bower_components/momentjs/min/moment.min',
+            'sf.b2c.mall.business.config': 'scripts/config/sf.b2c.mall.business.<%= config.target %>.config'
+          },
+          include: [
+            "sf.b2c.mall.business.config",
+            'sf.b2c.mall.page.bindaccount'
+          ],
+          insertRequire: ['sf.b2c.mall.page.bindaccount']
+        }
+      }
     }
   });
 
