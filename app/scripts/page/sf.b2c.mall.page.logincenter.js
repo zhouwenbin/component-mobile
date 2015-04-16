218a05ea103b8f4ec79ce69bb935122fb3ad9169
zhang.ke <zhang.ke@sf-express.com>
2015-04-16 22:11:48 +0800

    []

--- app/scripts/page/sf.b2c.mall.page.logincenter.js
+++ app/scripts/page/sf.b2c.mall.page.logincenter.js
@@ -23,6 +23,7 @@ define(
        * [init 初始化]
        */
       init: function() {
+        alert("center");
         var params = can.deparam(window.location.search.substr(1));
         var tag = params.tag;
         var redirectUrl = params.redirectUrl;
@@ -50,7 +51,9 @@ define(
         partnerLogin
           .sendRequest()
           .done(function(loginData) {
-            //alert(loginData.csrfToken);
+            alert(loginData.csrfToken);
+            alert(loginData.tempToken);
+            alert(tmpl);
 
             if (loginData.csrfToken) {
               store.set('type', type);
