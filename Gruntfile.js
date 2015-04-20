5fd4b760e5127e8809fec5687e52eeba1aca475b
jnotnull <252210289@qq.com>
2015-04-20 21:57:56 +0800

    修改头部尾部

--- Gruntfile.js
+++ Gruntfile.js
@@ -385,7 +385,9 @@ module.exports = function (grunt) {
         cwd: '<%= config.app %>',
         dest: '<%= config.dist %>',
         src: [
-          '*.html'
+          '*.html',
+          'header/*.html',
+          'footer/*.html'
         ],
         options:{
           process: function (content, srcpath) {
@@ -982,26 +984,7 @@ module.exports = function (grunt) {
           ],
           insertRequire: ['sf.b2c.mall.page.searchwarriorshare']
         }
-      },
-      getcoupon: {
-        options: {
-          preserveLicenseComments: false,
-          baseUrl: './app/',
-          out: './<%= config.tmp %>/concat/scripts/sf.b2c.mall.h5.page.getcoupon.js',
-          mainConfigFile: "./<%= config.app %>/scripts/sf.b2c.mall.require.config.js",
-          paths: {
-            'moment': '../bower_components/momentjs/min/moment.min',
-            'sf.b2c.mall.business.config': 'scripts/config/sf.b2c.mall.business.<%= config.target %>.config'
-          },
-          include: [
-            "sf.b2c.mall.business.config",
-            'sf.b2c.mall.page.getcoupon'
-          ],
-          insertRequire: ['sf.b2c.mall.page.getcoupon']
-        }
       }
-
-
 
 
     }
