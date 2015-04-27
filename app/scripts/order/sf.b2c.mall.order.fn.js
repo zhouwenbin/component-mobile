5f8e9ab08eb1522930288ee6fbebd1a1de643709
jnotnull <252210289@qq.com>
2015-04-27 20:28:13 +0800

    增加成功后处理

--- app/scripts/order/sf.b2c.mall.order.fn.js
+++ app/scripts/order/sf.b2c.mall.order.fn.js
@@ -95,7 +95,9 @@ define(
         WeixinJSBridge.invoke(
           'getBrandWCPayRequest', data,
           function(res) {
-            if (res.err_msg == "get_brand_wcpay_request:ok") {} // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+            if (res.err_msg == "get_brand_wcpay_request:ok") {
+              window.location.href = "http://m.sfht.com/orderlist.html";
+            } // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
           }
         );
       }
