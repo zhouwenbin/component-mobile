58d9199ffe6d617aa15ab4e0d654814af919a744
jnotnull <252210289@qq.com>
2015-05-11 17:21:29 +0800

    增加地址功能

--- app/scripts/component/sf.b2c.mall.component.addreditor.js
+++ app/scripts/component/sf.b2c.mall.component.addreditor.js
@@ -56,6 +56,7 @@ define('sf.b2c.mall.component.addreditor', [
       this.addressSaveClick();
     },
     "#backToOrder click": function() {
+      can.route.attr('tag', 'init');
       $(".order").show();
       $(".order-manager").show();
       $(".nav-c1, .nav-c2").show();
