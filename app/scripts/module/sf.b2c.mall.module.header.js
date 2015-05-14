23aaf7519bf72b59788d5d1102dabda29521191b
jnotnull <252210289@qq.com>
2015-05-14 21:02:44 +0800

    修改弹出广告

--- app/scripts/module/sf.b2c.mall.module.header.js
+++ app/scripts/module/sf.b2c.mall.module.header.js
@@ -17,18 +17,18 @@ define(
     var header = can.Control.extend({
 
       init: function() {
-        // this.setup($('body'));
+        this.setup($('body'));
 
-        // var template = can.view.mustache(this.getADHtml());
-        // $('body').append(template({}));
+        var template = can.view.mustache(this.getADHtml());
+        $('body').append(template({}));
 
-        // this.showAD();
+        this.showAD();
       },
 
       getADHtml: function() {
         return '<section class="banner-dialog" style="display:none">' +
           '<div class="mask"></div>' +
-          '<div class="banner-dialog-b center" id="banner-dialog"><span id="closebutton" class="icon icon15">关闭</span><ul><li>原价69元  新会员专享9元</li><li>每日限量200只(每日10:30开抢)</li></ul><a href="">查看详情</a></div>' +
+          '<div class="banner-dialog-b center" id="banner-dialog"><span id="closebutton" class="icon icon15">关闭</span></div>' +
           '</section>'
       },
 
