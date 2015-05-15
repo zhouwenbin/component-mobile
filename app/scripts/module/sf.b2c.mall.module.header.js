5f91b21061d68eeada461432908592c4bc24f21b
zhang.ke <zhang.ke@sf-express.com>
2015-05-15 11:18:10 +0800

    [520]

--- app/scripts/module/sf.b2c.mall.module.header.js
+++ app/scripts/module/sf.b2c.mall.module.header.js
@@ -37,7 +37,7 @@ define(
       },
 
       "#banner-dialog click": function() {
-        window.location.href = "http://m.sfht.com/detail/184.html";
+        window.location.href = "http://m.sfht.com/detail/520.html";
       },
 
       showAD: function() {
