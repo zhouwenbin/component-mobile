e567988cda270e0d9ba912143285ff617f0f8652
Michael.Lee <leewind19841209@gmail.com>
2015-05-21 12:48:45 +0800

    [dev]购物车更新数量

--- app/scripts/component/sf.b2c.mall.component.shoopingcat.js
+++ app/scripts/component/sf.b2c.mall.component.shoopingcat.js
@@ -26,9 +26,19 @@ define(
     // 注册服务端的appid
     SFFrameworkComm.register(3);
 
+    var LIMITED_PRICE = 1000;
+
     var PageShoppingCart = can.Control.extend({
 
       helpers: {
+
+        /**
+         * @description 商品情况是否属于可购买
+         * @param  {array} goodList   商品列表
+         * @param  {string} type      类型avail|disable
+         * @param  {object} options
+         * @return {function} 是否展示
+         */
         'sf-cart-type': function(goodList, type, options) {
           var array = goodList();
           var map = {
@@ -47,6 +57,11 @@ define(
           }
         },
 
+        /**
+         * @description 显示商品图片
+         * @param  {array} images 商品图片列表
+         * @return {string}       图片地址
+         */
         'sf-show-img': function(images) {
           var array = images();
           if (_.isArray(array) && array.length > 0) {
@@ -54,11 +69,31 @@ define(
           }
         },
 
-        'sf-selling-price': function(canUseActivityPrice activityPrice price) {
+        /**
+         * @description 通过活动价格/是否可食用活动价格/商品价格，来计算销售价格
+         * @param  {function} canUseActivityPrice 是否使用活动价格
+         * @param  {function} activityPrice       活动价格
+         * @param  {function} price               商品价格
+         * @return {int} 商品价格
+         */
+        'sf-selling-price': function(canUseActivityPrice, activityPrice, price) {
           if (canUseActivityPrice()) {
             return activityPrice() / 100;
           } else {
             return price() / 100;
+          }
+        },
+
+        /**
+         * @description 如果价格超过海关上线提示用户
+         * @param  {function} totalFee 总价格信息
+         * @return {function} 是否展示
+         */
+        'sf-is-over-limit': function(totalFee) {
+          if (totalFee() < LIMITED_PRICE) {
+            return options.inverse(options.contexts || this);
+          } else {
+            return options.fn(options.contexts || this);
           }
         }
       },
@@ -85,7 +120,7 @@ define(
             var params = this.getItemInfo(item)
           },
 
-          'removeitem': function(list) {
+          'removeitem': function(items) {
             var removeitem = new SFShopcartRemoveItem();
             var params = {
               itemIds: JSON.stringify(this.getItemIds(list))
@@ -115,12 +150,83 @@ define(
 
       },
 
-      getItemIds: function(itemList) {
+      getItemIds: function(items) {
 
       },
 
-      getItemInfo: function(itemId) {
+      getItemInfo: function(item) {
+        return {
+          itemId: item.itemId,
+          num: item.quantity
+        };
+      },
 
+      /**
+       * @description 购物车item数量减1
+       * @param  {object} $element 减号按钮对象
+       * @param  {event}  event    事件对象
+       * @return boolean
+       */
+      '.minus-num click': function($element, event) {
+        // 从上层dom中获取good信息
+        var good = $element.closet('li').data('good');
+
+        if (good.quantity > 0) {
+          good.quantity = good.quantity - 1;
+          this.requestFactory('updatenum', good);
+        }
+
+        return false;
+      },
+
+      /**
+       * @description 购物车item数量加1
+       * @param  {object} $element 减号按钮对象
+       * @param  {event}  event    事件对象
+       * @return boolean
+       */
+      '.plus-num click': function($element, event) {
+        // 从上层dom中获取good信息
+        var good = $element.closet('li').data('good');
+
+        if (good.quantity + 1 < good.limitQuantity) {
+          good.quantity = good.quantity + 1;
+          this.requestFactory('updatenum', good);
+        } else {
+          this.showAlert($element, good);
+        }
+
+        return false;
+      },
+
+      /**
+       * @description 购物车item数量变化
+       * @param  {object} $element 减号按钮对象
+       * @param  {event}  event    事件对象
+       * @return boolean
+       */
+      '.input-num change': function($element, event) {
+        var ask = $element.val();
+        var good = $element.closet('li').data('good');
+
+        if (ask < good.limitQuantity) {
+          this.requestFactory('updatenum', good);
+        } else {
+          $element.val(1);
+          this.showAlert($element, good);
+        }
+
+        return false;
+      },
+
+      showAlert: function($element, good) {
+        // 如果用户增加的数量大于限购数量，显示错误提示
+        var word = '';
+        _.each(good.limitDescList, function(value, key, list) {
+          word = word + value.desc + ' '
+        });
+
+        $element.closet('.text-error').text(word);
       },
 
       paint: function(data) {
