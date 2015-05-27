941111c254464c49670bf8cb1af63eb8a7ca21d7
Michael.Lee <leewind19841209@gmail.com>
2015-05-27 11:14:52 +0800

    [debug][CART-227]

--- app/scripts/module/sf.b2c.mall.module.header.js
+++ app/scripts/module/sf.b2c.mall.module.header.js
@@ -75,7 +75,7 @@ define(
         if (SFComm.prototype.checkUserLogin.call(this)) {
           window.location.href = href;
         } else {
-          window.location.href = hrefLogin;
+          window.location.href = hrefLogin + '?from='+ encodeURIComponent;
         }
       },
 
