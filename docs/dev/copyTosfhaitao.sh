c7e643d78d92d319dd84ef12b7244b0a45ea4a23
jnotnull <252210289@qq.com>
2015-06-04 16:56:55 +0800

    修改脚本

--- docs/dev/copyTosfhaitao.sh
+++ docs/dev/copyTosfhaitao.sh
@@ -1,28 +1,28 @@
-frompath=/Users/jiyanliang/haitao-b2c-h5
-topath=/Users/jiyanliang/sfhaitao
+frompath=/Users/jiyanliang/haitao-b2c-h5/.tmp/concat/scripts
+topath=/Users/jiyanliang/sfhaitao/www/order/scripts
 
 echo "copy from: "  $frompath  " to: "  $topath
 
 echo "copy order..."
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.gotopay.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.gotopay.js
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.order.detail.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.order.detail.js
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.order.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.order.js
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.order.list.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.order.list.js
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.paysuccess.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.paysuccess.js
+cp ${frompath}/sf.b2c.mall.h5.page.gotopay.js  ${topath}/sf.b2c.mall.h5.page.gotopay.js
+cp ${frompath}/sf.b2c.mall.h5.page.order.detail.js  ${topath}/sf.b2c.mall.h5.page.order.detail.js
+cp ${frompath}/sf.b2c.mall.h5.page.order.js  ${topath}/sf.b2c.mall.h5.page.order.js
+cp ${frompath}/sf.b2c.mall.h5.page.order.list.js  ${topath}/sf.b2c.mall.h5.page.order.list.js
+cp ${frompath}/sf.b2c.mall.h5.page.paysuccess.js  ${topath}/sf.b2c.mall.h5.page.paysuccess.js
 echo "copy order finished."
 
 echo "copy center..."
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.center.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.center.js
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.coupon.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.coupon.js
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.luckymoneyaccept.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.luckymoneyaccept.js
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.luckymoneyshare.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.luckymoneyshare.js
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.recaddrmanage.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.recaddrmanage.js
+cp ${frompath}/sf.b2c.mall.h5.page.center.js  ${topath}/sf.b2c.mall.h5.page.center.js
+cp ${frompath}/sf.b2c.mall.h5.page.coupon.js  ${topath}/sf.b2c.mall.h5.page.coupon.js
+cp ${frompath}/sf.b2c.mall.h5.page.luckymoneyaccept.js  ${topath}/sf.b2c.mall.h5.page.luckymoneyaccept.js
+cp ${frompath}/sf.b2c.mall.h5.page.luckymoneyshare.js  ${topath}/sf.b2c.mall.h5.page.luckymoneyshare.js
+cp ${frompath}/sf.b2c.mall.h5.page.recaddrmanage.js  ${topath}/sf.b2c.mall.h5.page.recaddrmanage.js
 echo "copy center finished."
 
 echo "copy detail..."
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.detail.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.detail.js
+cp ${frompath}/sf.b2c.mall.h5.page.detail.js  ${topath}/sf.b2c.mall.h5.page.detail.js
 echo "copy detail finished."
 
 echo "copy main..."
-cp ${frompath}/.tmp/concat/scripts/sf.b2c.mall.h5.page.main.js  ${topath}/www/order/scripts/sf.b2c.mall.h5.page.main.js
+cp ${frompath}/sf.b2c.mall.h5.page.main.js  ${topath}/sf.b2c.mall.h5.page.main.js
 echo "copy main finished."
