f188a8bcdcf03beda0d8aecd07219af2bbcd3373
Michael.Lee <leewind19841209@gmail.com>
2015-06-26 18:44:28 +0800

    [dev]在shop.html中需要引入sso

--- app/scripts/page/sf.b2c.mall.page.shop.js
+++ app/scripts/page/sf.b2c.mall.page.shop.js
@@ -9,10 +9,11 @@ define(
     'sf.b2c.mall.business.config',
     'sf.b2c.mall.component.search',
     'sf.b2c.mall.shop.detail',
-    'sf.b2c.mall.module.header'
+    'sf.b2c.mall.module.header',
+    'sf.b2c.mall.module.sso'
   ],
   function(can, $, Fastclick, SFFrameworkComm, SFFn, SFBusiness,
-           SFSearch, SFShopDetail, SFHeader) {
+           SFSearch, SFShopDetail, SFHeader, SFSso) {
     Fastclick.attach(document.body);
     SFFrameworkComm.register(3);
 
