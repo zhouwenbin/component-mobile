53c9e5291ded15f420f765a887964eef2cc6d2b1
TimGuan <gkqwt1990@gmail.com>
2015-06-29 14:17:42 +0800

    bug fix

--- app/scripts/module/sf.b2c.mall.module.exchangecode.js
+++ app/scripts/module/sf.b2c.mall.module.exchangecode.js
@@ -17,18 +17,6 @@ define(
 
         switcher.register('app', function() {
             var app = {
-                initialize: function() {
-                    this.bindEvents();
-                },
-
-                bindEvents: function() {
-                    document.addEventListener('deviceready', this.onDeviceReady, false);
-                },
-
-                onDeviceReady: function() {
-                    app.receivedEvent('deviceready');
-                },
-
                 /**
                  * @description 渲染html
                  * @param data
@@ -40,29 +28,28 @@ define(
                     this.element.html(html);
                 },
 
-                calculateExchangeCode: function(){
+                calculateExchangeCode: function() {
                     var _aid = $.fn.cookie('_aid') || '3';
                     var uinfo = $.fn.cookie(_aid + '_uinfo');
                     var arr = [];
                     if (uinfo) {
-                      arr = uinfo.split(',');
+                        arr = uinfo.split(',');
                     }
-                    var decimal = parseInt('ffffffff',16)-parseInt(arr[3])*41;
+                    var decimal = parseInt('ffffffff', 16) - parseInt(arr[3]) * 41;
                     return decimal.toString(16);
                 },
 
-                receivedEvent: function(id) {
+                initialize: function(id) {
                     var that = this;
-                    var renderData={};
+                    var renderData = {};
                     if (SFFrameworkComm.prototype.checkUserLogin.call(this)) {
-                         renderData.exchangeCode = calculateExchangeCode();
-                    }else{
+                        renderData.exchangeCode = calculateExchangeCode();
+                    } else {
                         renderData.exchangeCode = '请登入后查看';
                     }
                     $(".fill-exchangecode").html(renderHtml(renderData));
                 }
             };
-
             app.initialize();
         });
 
