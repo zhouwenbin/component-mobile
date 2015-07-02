7214eb5740fa8e2a3100b8657deff29fa3d5e0a7
Michael.Lee <leewind19841209@gmail.com>
2015-07-02 15:46:17 +0800

    [dev]修改app.html的领取方式

--- app/scripts/page/sf.b2c.mall.page.app.js
+++ app/scripts/page/sf.b2c.mall.page.app.js
@@ -3,13 +3,18 @@ define(
   [
     'can',
     'zepto',
-    'fastclick',
+    // 'fastclick',
     'sf.b2c.mall.framework.comm',
     'sf.util',
-    'zepto.fullpage'
+    'zepto.fullpage',
+    'store',
+    'sf.b2c.mall.business.config',
+    'sf.b2c.mall.widget.message',
+    'sf.b2c.mall.api.coupon.receiveCoupon'
+    // 'animate'
   ],
-  function(can, $, Fastclick, SFFrameworkComm, SFFn, Fullpage) {
-    Fastclick.attach(document.body);
+  function(can, $, SFFrameworkComm, SFFn, Fullpage, store, SFConfig, SFMessage, SFReceiveCoupon) {
+    // Fastclick.attach(document.body);
     SFFrameworkComm.register(3);
 
     var appPage = can.Control.extend({
@@ -17,22 +22,22 @@ define(
        * [init 初始化]
        */
       init: function() {
-        //this.initFullPage();
+        this.initFullPage();
         this.render();
         var that = this;
 
         //$("#openAddLink").click();
 
-        that.openApp('sfht://');
+        // that.openApp('sfht://');
 
 
-        if (SFFn.isMobile.WeChat()) {
-          $("#downloadAppBtn").attr("href", "http://a.app.qq.com/o/simple.jsp?pkgname=com.sfht.m");
-        } else if (SFFn.isMobile.iOS()) {
-          $("#downloadAppBtn").attr("href", "https://itunes.apple.com/us/app/hai-tao-fa-xian/id983956499?mt=8");
-        } else if (SFFn.isMobile.Android()) {
-          $("#downloadAppBtn").attr("href", "http://img.sfht.com/ios/sfht_sfhaitao.apk");
-        }
+        // if (SFFn.isMobile.WeChat()) {
+        //   $("#downloadAppBtn").attr("href", "http://a.app.qq.com/o/simple.jsp?pkgname=com.sfht.m");
+        // } else if (SFFn.isMobile.iOS()) {
+        //   $("#downloadAppBtn").attr("href", "https://itunes.apple.com/us/app/hai-tao-fa-xian/id983956499?mt=8");
+        // } else if (SFFn.isMobile.Android()) {
+        //   $("#downloadAppBtn").attr("href", "http://img.sfht.com/ios/sfht_sfhaitao.apk");
+        // }
 
         // setTimeout(function(){
         //   if (SFFn.isMobile.iOS()) {
@@ -81,8 +86,91 @@ define(
       render: function() {
         var that = this;
       }
+    });
 
+    var getCoupon =can.Control.extend({
+      /**
+       * @override
+       * @description 初始化方法
+       */
+      init: function() {
+        var that = this;
+
+        $("[data-name='cms-fill-coupon']").click(function(targetElement) {
+          if (!SFFrameworkComm.prototype.checkUserLogin.call(this)) {
+            new SFMessage(null, {
+              'tip': '抱歉！需要登录后才可以领取优惠券！',
+              'type': 'success',
+              'okFunction': function(){
+                window.location.href = "http://m.sfht.com/login.html?from=" + escape(window.location.href);
+              }
+            });
+            return false;
+          }
+
+          var params = {
+            bagId: $(targetElement.target).data('cms-couponbagid'),
+            type: $(targetElement.target).data('cms-coupontype')
+          }
+          var needSms = $(targetElement.target).data('needsms');
+          var smsCon = $(targetElement.target).data('smscon');
+          if (needSms) {
+            params.needSms = needSms;
+          }
+          if (smsCon) {
+            params.smsCon = smsCon;
+          }
+
+          that.receiveCpCodeData(params);
+        });
+
+        return false;
+      },
+
+      errorMap: {
+        "11000020": "卡券不存在",
+        "11000030": "卡券已作废",
+        "11000050": "卡券已领完",
+        "11000100": "您已领过该券",
+        "11000130": "卡包不存在",
+        "11000140": "卡包已作废"
+      },
+
+      receiveCpCodeData: function(params) {
+        params.receiveChannel = 'B2C';
+        params.receiveWay = 'ZTLQ';
+        var that = this;
+        var receiveCouponData = new SFReceiveCoupon(params);
+        return can.when(receiveCouponData.sendRequest())
+          .done(function(userCouponInfo) {
+            new SFMessage(null, {
+              'tip': '领取成功！',
+              'type': 'success'
+            });
+
+            if (SFFn.isMobile.WeChat()) {
+
+              window.location.href = "http://a.app.qq.com/o/simple.jsp?pkgname=com.sfht.m";
+              // $("#downloadAppBtn").attr("href", "http://a.app.qq.com/o/simple.jsp?pkgname=com.sfht.m");
+            } else if (SFFn.isMobile.iOS()) {
+
+              window.location.href = "https://itunes.apple.com/us/app/hai-tao-fa-xian/id983956499?mt=8";
+              // $("#downloadAppBtn").attr("href", "https://itunes.apple.com/us/app/hai-tao-fa-xian/id983956499?mt=8");
+            } else if (SFFn.isMobile.Android()) {
+
+              window.location.href = "http://img.sfht.com/ios/sfht_sfhaitao.apk";
+              // $("#downloadAppBtn").attr("href", "http://img.sfht.com/ios/sfht_sfhaitao.apk");
+            }
+          })
+          .fail(function(error) {
+            new SFMessage(null, {
+              'tip': that.errorMap[error] || '领取失败',
+              'type': 'error'
+            });
+          });
+      }
     });
 
     new appPage();
+    new getCoupon();
   })
