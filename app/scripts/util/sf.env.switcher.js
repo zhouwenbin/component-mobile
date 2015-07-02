20709b960e60c0415421760963ef6d1c7cf58756
Michael.Lee <leewind19841209@gmail.com>
2015-07-02 11:38:41 +0800

    [dev]修复环境切换问题

--- app/scripts/util/sf.env.switcher.js
+++ app/scripts/util/sf.env.switcher.js
@@ -34,6 +34,14 @@ define(
             return SFFn.isMobile.AlipayChat();
           },
 
+          'localapp': function () {
+            return SFFn.isMobile.localApp();
+          },
+
+          'onlineapp': function () {
+            return SFFn.isMobile.onlineApp();
+          },
+
           // @todo app的判断逻辑预留
           'app': function() {
             // @deprecated
@@ -41,26 +49,17 @@ define(
             // return !!cordova;
 
             return SFFn.isMobile.APP();
-          },
-
-          'localapp': function () {
-            return SFFn.isMobile.localApp();
-          },
-
-          'onlineapp': function () {
-            return SFFn.isMobile.onlineApp();
           }
         }
 
-        var env = null;
+        var env = [];
         for (var i in loop) {
           if (loop[i]()) {
-            env = i;
-            break;
+            env.push(i);
           }
         }
 
-        return env ? env : defaultEnv;
+        return env.length > 0 ? env : [defaultEnv];
       },
 
       register: function(env, callback) {
@@ -69,49 +68,21 @@ define(
         }
       },
 
-      /**
-       * [registerManyToOne 注册一个回调到多个环境中]
-       * @param  {[type]}   env      [description]
-       * @param  {Function} callback [description]
-       * @return {[type]}            [description]
-       */
-      registerOneCallbackToManyEnv: function(envs, callback) {
-        var that = this;
-
-        var envArr = envs.split(",");
-
-        _.each(envArr, function(item) {
-          that.register(item, callback);
-        })
-      },
-
-      /**
-       * [registerManyToOne 除去某个环境，其他环境都一个回调]
-       * @param  {[type]}   env      [description]
-       * @param  {Function} callback [description]
-       * @return {[type]}            [description]
-       */
-      registerOneCallbackExceptEnv: function(envs, callback) {
-        var that = this;
-
-        var envArr = envs.split(",");
-
-        _.each(setting, function(item) {
-          _.each(envArr, function(notRegisterItem) {
-            if (notRegisterItem != item){
-              that.register(item, callback);
-            }
-          })
-        })
-      },
-
       go: function() {
-        var fn = this.options.group[this.options.env] || this.options.group[defaultSetting];
-        if (_.isFunction(fn)) {
-          fn.call(this);
+        var runFn = this.options.group[defaultSetting];
+
+        for(var i in this.options.env){
+          var fn = this.options.group[this.options.env[i]];
+          if (_.isFunction(fn)) {
+            runFn = fn;
+            break;
+          }
+        }
+
+        if (_.isFunction(runFn)) {
+          runFn.call(this);
         }
       }
     });
-
   }
-)
+);
