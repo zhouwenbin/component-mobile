6dc8a85e0f77b00c7825565b9dc6e34e563b0f7f
sfyangkai <kaiyang1@sf-express.com>
2015-07-09 17:11:10 +0800

    fix

--- app/scripts/sf.b2c.mall.require.config.js
+++ app/scripts/sf.b2c.mall.require.config.js
@@ -90,8 +90,6 @@ requirejs.config({
         "sf.b2c.mall.api.user.rqCash": "scripts/api/sf.b2c.mall.api.user.rqCash",
         "sf.b2c.mall.api.user.getCashActInfo": "scripts/api/sf.b2c.mall.api.user.getCashActInfo",
         "sf.b2c.mall.api.user.bindAliAct": "scripts/api/sf.b2c.mall.api.user.bindAliAct",
-        "sf.b2c.mall.api.user.getVoteNum": "scripts/api/sf.b2c.mall.api.user.getVoteNum",
-        "sf.b2c.mall.api.user.vote": "scripts/api/sf.b2c.mall.api.user.vote",
 
         "sf.b2c.mall.api.minicart.getTotalCount": "scripts/api/sf.b2c.mall.api.minicart.getTotalCount",
         "sf.b2c.mall.api.shopcart.addItemToCart": "scripts/api/sf.b2c.mall.api.shopcart.addItemToCart",
@@ -134,6 +132,7 @@ requirejs.config({
         "sf.b2c.mall.api.cp.generateSubjectUrlWidthSCM": "scripts/api/sf.b2c.mall.api.cp.generateSubjectUrlWidthSCM",
         "sf.b2c.mall.api.integral.getUserIntegralLog": "scripts/api/sf.b2c.mall.api.integral.getUserIntegralLog",
         "sf.b2c.mall.api.user.getVoteNum": "scripts/api/sf.b2c.mall.api.user.getVoteNum",
+        "sf.b2c.mall.api.user.vote": "scripts/api/sf.b2c.mall.api.user.vote",
         "placeholders": "bower_components/Placeholders/build/placeholders",
         "moment": "bower_components/momentjs/min/moment.min",
         "moment-zh-cn": "bower_components/momentjs/locale/zh-cn",
