93c6916fc0387db4ac1098a34b94e1c3c69bfb99
Michael.Lee <leewind19841209@gmail.com>
2015-07-12 22:21:45 +0800

    [dev]

--- app/scripts/page/sf.b2c.mall.page.ouba.js
+++ app/scripts/page/sf.b2c.mall.page.ouba.js
@@ -56,12 +56,18 @@ define(
                 $('.wp-inner').fullpage();
                 $.fn.fullpage.stop();
 
-                new PageResponse({
-                    class : 'page',     //模块的类名，使用class来控制页面上的模块(1个或多个)
-                    mode : 'contain',     // auto || contain || cover
-                    width : '640',      //输入页面的宽度，只支持输入数值，默认宽度为320px
-                    height : '1006'      //输入页面的高度，只支持输入数值，默认高度为504px
+                var switcher = new SFSwitcher();
+                switcher.register('app', function(){
+                    new PageResponse({
+                        class : 'page',     //模块的类名，使用class来控制页面上的模块(1个或多个)
+                        mode : 'contain',     // auto || contain || cover
+                        width : '640',      //输入页面的宽度，只支持输入数值，默认宽度为320px
+                        height : '1006'      //输入页面的高度，只支持输入数值，默认高度为504px
+                    });
                 });
+
+                switcher.register('web', function(){});
+                switcher.go();
 
                 $("#nextPage").css("display","none");
                 this.getTicketList();
