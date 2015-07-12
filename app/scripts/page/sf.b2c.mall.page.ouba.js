d7f840374e0478e80ec1ad5c251ef8bdbcdfcf6f
Michael.Lee <leewind19841209@gmail.com>
2015-07-12 21:51:43 +0800

    [dev]fixed

--- app/scripts/page/sf.b2c.mall.page.ouba.js
+++ app/scripts/page/sf.b2c.mall.page.ouba.js
@@ -61,10 +61,10 @@ define(
 
                 new PageResponse({
                     class : 'page',     //模块的类名，使用class来控制页面上的模块(1个或多个)
-                    mode : 'auto',     // auto || contain || cover
+                    mode : 'contain',     // auto || contain || cover
                     width : '640',      //输入页面的宽度，只支持输入数值，默认宽度为320px
-                    height : '1008'      //输入页面的高度，只支持输入数值，默认高度为504px
-                })
+                    height : '1006'      //输入页面的高度，只支持输入数值，默认高度为504px
+                });
 
                 $("#nextPage").css("display","none");
                 this.getTicketList();
