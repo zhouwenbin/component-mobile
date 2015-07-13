7fb7c4a89051504fda1578a3509219ebbb31c10c
zhang.ke <zhang.ke@sf-express.com>
2015-07-13 15:23:19 +0800

    [optimize.search]base

--- app/scripts/search/sf.b2c.mall.search.searchinput.js
+++ app/scripts/search/sf.b2c.mall.search.searchinput.js
@@ -5,18 +5,19 @@ define('sf.b2c.mall.search.searchinput', [
     'can',
     'underscore',
     'store',
-    'zepto.cookie',
-    'sf.helpers',
-    'sf.b2c.mall.framework.comm',
-    'sf.b2c.mall.business.config',
     'sf.b2c.mall.widget.loading',
     'sf.b2c.mall.api.search.hotKeywords',
     "text!template_search_searchinput"
   ],
-  function($, can, _, store, cookie, helpers, SFComm, SFConfig,
-           SFLoading, SFHotKeywords,
-           template_search_searchinput) {
+  function($, can, _, store, SFLoading, SFHotKeywords, template_search_searchinput) {
+
+  var STORE_HISTORY_LIST = "searchhistories";
+  var HISTORY_SIZE = 10;
+  var KEYWORD_SIZE = 10;
+
   return can.Control.extend({
+
+  	loading: new SFLoading(),
 
     helpers: {
       'sf-firstImg': function(imageList, options) {
@@ -34,8 +35,8 @@ define('sf.b2c.mall.search.searchinput', [
      * @param  {Object} options 传递的参数
      */
     init: function(element, options) {
-    	new SFLoading();
-    	this.initHotKeywords();
+      this.loading.show();
+      this.render();
     },
 
 		/**
@@ -44,23 +45,38 @@ define('sf.b2c.mall.search.searchinput', [
      */
     initHotKeywords: function() {
     	var that = this;
-    	var SFHotKeywords = new SFHotKeywords({size: 10});
-    	return SFHotKeywords.sendRequest()
+    	var sfHotKeywords = new SFHotKeywords({"size": KEYWORD_SIZE});
+    	return sfHotKeywords.sendRequest()
     		.done(function(hotKeywords) {
     			that.renderData.attr("hotKeywordList", hotKeywords);
     		});
     },
 
-    render: function(shopId) {
+		/**
+     * @description 从服务器端获取数据
+     * @param searchData
+     */
+    initHistories: function() {
+    	var that = this;
+			that.renderData.attr("historyList", store.get(STORE_HISTORY_LIST));
+    },
+
+    render: function() {
       var that = this;
-      can.when(this.initSearchShopInfo(shopId))
-        .done(function(){
+      
+      this.initHistories();
+      that.renderHtml();
+      return;
+      can.when(this.initHotKeywords())
+        .done(function() {
           that.renderHtml();
         })
-        .fail(function() {
-          that.goto404();
+        .always(function() {
+          that.loading.hide();
         });
-    },
+
+
+    }, 
 
     /**
      * @description 渲染html
@@ -68,9 +84,37 @@ define('sf.b2c.mall.search.searchinput', [
      */
     renderHtml: function(data) {
       //渲染页面
-      var renderFn = can.mustache(template_shop_detail);
+      var renderFn = can.mustache(template_search_searchinput);
       var html = renderFn(data || this.renderData, this.helpers);
       this.element.html(html);
+    },
+
+    "#searchInput keydown": function(element, event) {
+      if (event.keyCode != 13) {
+        return;
+      }
+
+      var keyword = $(element).val();
+      this.saveHistories(keyword);
+      this.gotoSearchPage(keyword);
+    },
+
+    gotoSearchPage: function(keyword) {
+      window.location.href = "/search.html?keyword=" + keyword;
+    },
+
+    saveHistories: function(keyword) {
+      var histories;
+      if (!this.renderData.historyList) {
+        histories = [];
+      } else {
+        histories = this.renderData.historyList.serialize();
+      }
+      histories.splice(0, 0, keyword);
+      if (histories.length > HISTORY_SIZE) {
+        histories = histories.slice(0, HISTORY_SIZE);
+      }
+      store.set(STORE_HISTORY_LIST, histories);
     }
   });
 })
