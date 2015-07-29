ec8629a17a45ac2044672da117a4abf1fdad15f9
zhang.ke <zhang.ke@sf-express.com>
2015-07-29 10:16:19 +0800

    [promoter]base

--- app/scripts/page/sf.b2c.mall.page.tmlg.js
+++ app/scripts/page/sf.b2c.mall.page.tmlg.js
@@ -8,16 +8,11 @@ define(
     'sf.util',
     'sf.b2c.mall.business.config',
   	'sf.b2c.mall.widget.message',
-  	'sf.b2c.mall.api.user.regstAocart4Pmter'
+  	'sf.b2c.mall.api.user.exchangeToken'
   ],
-  function(can, $, Fastclick, SFFrameworkComm, SFFn, SFBusiness, SFMessage, SFRegstAocart4Pmter) {
+  function(can, $, Fastclick, SFFrameworkComm, SFFn, SFBusiness, SFMessage, SFExchangeToken) {
     Fastclick.attach(document.body);
     SFFrameworkComm.register(3);
-
-    errorMap = {
-    	"1000020": "账户已注册",
-    	"1000470": "邀请人并非推广员"
-    };
 
     var tmlgPage = can.Control.extend({
 
@@ -26,62 +21,35 @@ define(
        */
       init: function() {
       	var that = this;
-        this.render();
+
+        var params = can.deparam(window.location.search.substr(1));
+        var tempToken = params.tt;
+        if (tempToken) {
+          this.initExchangeToken(tempToken);
+        }
       },
 
-      /**
-       * [render 渲染]
-       */
-      render: function() {
-      },
 
       /**
        * @author zhang.ke
        * @description 从服务器端获取数据
        */
-	    initRegstAocart4Pmter: function(inviterMobile, inviteeMobile, itemIds) {
+	    initExchangeToken: function(tempToken) {
 	      var that = this;
-	      var regstAocart4Pmter = new SFRegstAocart4Pmter({
-	      	"inviterMobile": inviterMobile,
-	      	"inviteeMobile": inviteeMobile,
-	      	"itemIds": itemIds,
+	      var exchangeToken = new SFExchangeToken({
+	      	"tempToken": tempToken
 	      });
-	      return regstAocart4Pmter.sendRequest()
+	      return exchangeToken.sendRequest()
 	        .done(function(result) {
+            window.location.href = "/shoppingcart.html";
 	        })
 	        .fail(function(error) {
-	        	new SFMessage(null, {
-		          'tip': errorMap[error],
-		          'type': 'error'
-		        });
+            new SFMessage(null, {
+              'tip': "登录信息已过期",
+              'type': 'error'
+            });
 	        });
 	    },
-
-	    "#submit-form-btn click": function() {
-
-	    },
-
-      submitForm: function() {
-
-      	var promoterPhone = $("[name=promoterPhone]").val();
-      	var customerPhone = $("[name=customerPhone]").val();
-      	var itemIds = $("[name=itemIds]").val();
-
-      	can.when(this.initRegstAocart4Pmter(promoterPhone, customerPhone, itemIds))
-      		.done(function(result) {
-      			if (result.value) {
-      				new SFMessage(null, {
-			          'tip': '操作成功!',
-			          'type': 'error'
-	        		});
-      			} else {
-      				new SFMessage(null, {
-			          'tip': '操作失败!',
-			          'type': 'error'
-	        		});
-      			}
-      		})
-      },
 
 	    trim: function(str) {
 	      return str.replace(/(^\s*)|(\s*$)/g, "");
@@ -89,5 +57,5 @@ define(
 
     });
 
-    new tmlgrPage();
+    new tmlgPage();
   })
