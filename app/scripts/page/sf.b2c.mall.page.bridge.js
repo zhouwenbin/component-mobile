dc25edc2e12c2a33f91c1c92887e31919fde3dde
Guo Yanhao <guoyanhao@sf-express.com>
2015-07-30 16:28:51 +0800

    [dev]

--- app/scripts/page/sf.b2c.mall.page.bridge.js
+++ app/scripts/page/sf.b2c.mall.page.bridge.js
@@ -4,28 +4,41 @@ define(
   'sf.b2c.mall.page.bridge',
 
   [
+    "zepto",
     "sf.bridge"
   ],
 
-  function () {
+  function ($) {
 
-    setTimeout(function() {
-      var params = {
-        "subject": '哈哈哈哈',
-        "description": '哈哈哈哈哈',
-        "imageUrl": 'https://ss0.bdstatic.com/k4oZeXSm1A5BphGlnYG/icon/10256.png',
-        "url": 'https://www.baidu.com/'
-      }
+    // setTimeout(function() {
+      // var params = {
+      //   "subject": '哈哈哈哈',
+      //   "description": '哈哈哈哈哈',
+      //   "imageUrl": 'https://ss0.bdstatic.com/k4oZeXSm1A5BphGlnYG/icon/10256.png',
+      //   "url": 'https://www.baidu.com/'
+      // }
 
-      var success =function () {
+      var success =function (data) {
         alert('success')
+        alert(JSON.stringify(data))
       }
 
-      var error = function () {
+      var error = function (data) {
         alert('error')
+        alert(JSON.stringify(data))
       }
 
-      window.bridge.run('SocialSharing', 'share', params, success, error)
-    }, 2000);
+      $('#runner').on('click', function () {
+        var plugin = $('#pluginName').val()
+        var method = $('#methodName').val()
+
+        var paramsStr = $('#params').val()
+        var params = JSON.parse(paramsStr);
+
+        window.bridge.run(plugin, method, params, success, error)
+      });
+
+      // window.bridge.run('SocialSharing', 'share', params, success, error)
+    // }, 2000);
 
   });
