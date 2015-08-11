041c88deca65b28fb183aad62b69424089ae59e8
王峰 <wangfeng@ifunq.com>
2015-08-11 11:38:23 +0800

    lazyload

--- app/scripts/module/sf.b2c.mall.module.imagelazyload.js
+++ app/scripts/module/sf.b2c.mall.module.imagelazyload.js
@@ -6,12 +6,6 @@ define(
     'lazyload'
   ],
 
-  function(can, $, _) {
-      $("img.imgLazyload").show().lazyload({
-          placeholder : "http://img.sfht.com/sfht/1.1.7/img/loading.gif",
-           // 设置下张图片到达前200px加载,逐个加载
-           // threshold : 200,
-          //插件等图像完全加载和调用show()，当有图片有在窗口范围内的,逐个加载
-          effect : "fadeIn"
-      });
+  function(can, $, _, lazyload) {
+      lazyload.init();
   });
