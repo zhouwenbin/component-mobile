5342e30498870edd06325650981989b0442375ac
王峰 <wangfeng@ifunq.com>
2015-08-18 19:44:48 +0800

    [dev]fulishe

--- app/scripts/page/sf.b2c.mall.page.welfare.js
+++ app/scripts/page/sf.b2c.mall.page.welfare.js
@@ -17,10 +17,15 @@ define(
       SFFrameworkComm.register(3);
       var loadingCtrl = new SFLoading();
       can.route.ready();
+
       var welfareList = can.Control.extend({
         itemObj: new can.Map({}),
         '{can.route} change': function() {
           this.render();
+        },
+
+        helpers: function() {
+
         },
 
         init: function() {
@@ -42,71 +47,49 @@ define(
         stepCates: function(data) {
           var renderFn = can.view.mustache(template_welfare_list);
           var html = renderFn(data);
-          // var str = JSON.stringify(data);
           this.element.html(html);
           loadingCtrl.hide();
-
         },
+
         instaceCatesCont: function(data) {
         },
+
         initLoadDataEvent: function() {
           this.loadingData();
         },
 
         loadingData: function(cparams) {
           var that = this;
-          var params = {
-            "pId": 0
-          };
+          var params = {};
           var sFgetUserTaskList =new SFgetUserTaskList(params);
           can.when(sFgetUserTaskList.sendRequest()).done(function(data) {
-            console.log(data)
-            that.itemObj.attr(data)
-            that.stepCates(that.itemObj)
-
+            that.itemObj.attr(data);
+            that.stepCates(that.itemObj);
+            console.log(data);
           }) .fail(function(error) {
             console.error(error);
           });
         },
-        '.listLi-3 click': function($element, event) {
-            if (!SFFrameworkComm.prototype.checkUserLogin.call(this)) {
+
+        '.joinBtns click': function($element, event) {
+          if (!SFFrameworkComm.prototype.checkUserLogin.call(this)) {
             window.location.href ="http://m.sfht.com/login.html";
             return false;
           }
-          var id=$element.attr("cate-p1-id");
-          if(exId==id)return;
-          var success = function() {
-            window.location.reload();
-          };
+          if($element.hasClass('direct')){
+            $element.attr({
+              href: 'javascript:;'
+            });
 
-          var error = function() {
-            // @todo 错误提示
+            var taskId = $element.closest('li').attr('data-tab');
+            var sFdirectReceiveAward = new SFdirectReceiveAward({taskId: taskId});
+            sFdirectReceiveAward.sendRequest().done(function(taskId){
+              alert(taskId.text)
+            }) .fail(function(error){
+              console.log(error)
+            })
           }
-          CategoryFn.getCate(exId,id, success, error);
-          exId=id;
         },
-        // '.getSecondCate click':function($element, event) {
-        //   var id=$element.attr("data-item-id");
-        //   var parentLi=$element.parent();
-        //   var spanStatus=$element.find(".category-iconarrow-close");
-        //   var status=spanStatus.length;
-        //     spanStatus.toggleClass("active");
-        //     parentLi.toggleClass("boder-li");
-        //   parentLi.find(".category-content-p2").toggleClass('active');
-        //   //parentLi.find(".category-content-p2").toggleClass('active');
-        //   if($.inArray(id, exSencondIds)>-1){
-        //     return;
-        //   }
-        //   var success = function() {
-        //     window.location.reload();
-        //   };
-
-        //   var error = function() {
-        //     // @todo 错误提示
-        //   }
-        //   CategoryFn.getSecondCate(parentLi,exSencondIds,id, success, error);
-
-        // }
        });
       new welfareList('body');
   });
