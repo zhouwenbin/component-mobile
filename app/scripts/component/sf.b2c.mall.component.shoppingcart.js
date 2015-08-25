50bf0998110f27661a7405464324076c64cbbc4f
Michael.Lee <leewind19841209@gmail.com>
2015-08-25 15:53:23 +0800

    [dev]

--- app/scripts/component/sf.b2c.mall.component.shoppingcart.js
+++ app/scripts/component/sf.b2c.mall.component.shoppingcart.js
@@ -605,7 +605,9 @@ define(
         var switcher = new SFSwitcher();
 
         switcher.register('web', _.bind(function() {
-          this.element.html(html);
+          this.element.empty(html);
+          this.element.append(html);
+
           can.trigger(window, 'updateCart');
           new SFWidgetCartNumber();
           loadingCtrl.hide();
@@ -614,7 +616,8 @@ define(
         switcher.register('app', _.bind(function() {
 
           if (this.options.data.scopeGroups.length > 0 || SFFn.isMobile.Android()) {
-            this.element.html(html);
+            this.element.empty(html);
+            this.element.append(html);
           }
 
           can.trigger(window, 'updateCart');
