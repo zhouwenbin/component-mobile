de64e1371ecaa063d7543ada6330c5c1cef0a06c
Michael.Lee <leewind19841209@gmail.com>
2015-08-25 15:45:53 +0800

    Revert "[dev]添加调试信息"
    This reverts commit 3d8e63034497b39dd71d8c881bae960909918f30.

--- app/scripts/component/sf.b2c.mall.component.shoppingcart.js
+++ app/scripts/component/sf.b2c.mall.component.shoppingcart.js
@@ -412,7 +412,6 @@ define(
        * @return
        */
       '.remove-item-btn click': function($element, event) {
-        console.log('.remove-item-btn click')
         // 从上层dom中获取good信息
         var good = $element.closest('li').data('good');
         this.requestFactory('removeitem', [good]);
@@ -425,7 +424,6 @@ define(
        * @return boolean
        */
       '.minus-num click': function($element, event) {
-        console.log('.minus-num click')
         // 从上层dom中获取good信息
         var good = $element.closest('li').data('good');
 
@@ -444,7 +442,6 @@ define(
        * @return boolean
        */
       '.plus-num click': function($element, event) {
-        console.log('.plus-num click')
         // 从上层dom中获取good信息
         var good = $element.closest('li').data('good');
 
