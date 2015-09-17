bc7f06c8e26a8709e9c690785bd9a358123a960c
aicekiller <1017634520@qq.com>
2015-09-17 11:51:50 +0800

    debug

--- app/scripts/order/sf.b2c.mall.order.iteminfo.js
+++ app/scripts/order/sf.b2c.mall.order.iteminfo.js
@@ -343,10 +343,12 @@ define('sf.b2c.mall.order.iteminfo', [
             });
             return orderRender.sendRequest()
                 .done(function(orderRenderItem) {
-
+                    alert(1);
                     that.itemObj.attr(orderRenderItem);
                     //积分抵扣多少钱
-                    that.itemObj.attr('equalMoney', that.itemObj.attr('integral') / that.itemObj.attr('proportion'));
+                    alert(that.itemObj.attr('integral'));
+                    alert(that.itemObj.attr('proportion'));
+                    that.itemObj.attr('equalMoney', that.itemObj.attr('integral') / that.itemObj.attr('proportion') || 0);
 
                 })
                 .fail();
