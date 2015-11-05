b2f6a85b35ebd9d6ea15f2e5a3ea4cdb9876c275
zhouwenbin <330759294@qq.com>
2015-11-05 14:49:09 +0800

    团详情

--- app/static/js/tuan.js
+++ app/static/js/tuan.js
@@ -4,4 +4,13 @@ $(function(){
   	$('.tuantab li').eq(index).addClass('active').siblings().removeClass('active');
   	$('.tuantab-content').eq(index).addClass('active').siblings().removeClass('active');
   })
+
+  $('.tuanbtn .btn').click(function(){
+  	$('.tuandialog').removeClass('hide');
+  })
+
+  $('.tuandialog .btn').click(function(){
+  	$('.tuandialog').addClass('hide');
+  	$('.tuanshare').removeClass('hide');
+  })
 })
