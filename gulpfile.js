e38983388c645c1105f90ae296ae7f5c565981bc
zhouwenbin <330759294@qq.com>
2015-11-18 20:10:11 +0800

    样式指南

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
+var fs = require('fs');
 var gulp = require('gulp');
 var svgSprite = require("gulp-svg-sprites");
 
 gulp.task('default', function () {
-    return gulp.src('app/static/icon/svg/*.svg')
+    gulp.src('app/static/icon/svg/*.svg')
         .pipe(svgSprite({mode: "symbols"}))
         .pipe(gulp.dest("app/static/icon/inlinesvg"))
+});
+gulp.task('guild', function () {
+    var postcss = require('gulp-postcss');
+    var processedCSS = fs.readFileSync('./app/static/css/pages/common/common.css', 'utf-8');
+    return gulp.src('./app/static/css/pages/common/common.css')
+        .pipe(postcss([
+            require('postcss-style-guide')({
+                name: "样式指南",
+                processedCSS: processedCSS
+            })
+        ]))
+        .pipe(gulp.dest('./docs'));
 });
