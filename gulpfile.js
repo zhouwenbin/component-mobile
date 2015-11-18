350040e03d10d12493c6e4a7838e15ae3d0a56a4
zhouwenbin <330759294@qq.com>
2015-11-18 15:45:51 +0800

    移动目录

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var svgSprite = require("gulp-svg-sprites");
 
 gulp.task('default', function () {
-    return gulp.src('app/static/svg/*.svg')
+    return gulp.src('app/static/icon/svg/*.svg')
         .pipe(svgSprite({mode: "symbols"}))
-        .pipe(gulp.dest("app/static/inlinesvg"))
+        .pipe(gulp.dest("app/static/icon/inlinesvg"))
 });
