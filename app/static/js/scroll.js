8138d6ecb3d0555d8b8930153369e4f20a019b87
lifanfan <lifanfan@sf-express.com>
2015-11-30 17:52:40 +0800

    tab nav切换

--- app/static/js/scroll.js
+++ app/static/js/scroll.js
@@ -3,8 +3,12 @@ $(function(){
   var bannerGoodsWidth = $('.banner-goods li').width();
   $('.banner-goods .new-goods-list').width((bannerGoodsWidth + 16) * bannerGoodsNum);
   new IScroll('.banner-goods', { eventPassthrough: true,scrollX: true});
-  var bannerGoodsNum = $('.baby-times-ct li').length;
-  var bannerGoodsWidth = $('.baby-times-ct li').width();
-  $('.baby-times-ct ul').width(bannerGoodsWidth * bannerGoodsNum);
-  new IScroll('.baby-times-ct', { eventPassthrough: true,scrollX: true});
+  
+  var domWidth = 0;
+  $.each($('.tabnav-box li'),function(key,value){
+  	domWidth += $(value).width() + 44 ;
+  })
+  console.log(domWidth);
+  $('.tabnav-box ul').width(domWidth);
+  new IScroll('.tabnav-box', { eventPassthrough: true,scrollX: true});
 })
